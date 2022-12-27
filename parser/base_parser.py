b8c9e1bc52c2cd2cbd4d83d00bd14e95e280a9cb
Daniil Zuev <daniilzuev216@gmail.com>
2022-12-27 17:16:35 +0300

    add new method for base_parser

--- parser/base_parser.py
+++ parser/base_parser.py
@@ -4,9 +4,29 @@ from abc import ABC, abstractmethod
 class BaseParser(ABC):
 
     @abstractmethod
-    def parse_category_site(self, body: str) -> None:
+    def get_category_site_response(self):
         pass
 
     @abstractmethod
-    def get_category_site(self):
+    def get_category_products_list_response(self):
+        pass
+
+    @abstractmethod
+    def get_shop_information_response(self):
+        pass
+
+    @abstractmethod
+    def get_city_shop_response(self):
+        pass
+
+    @abstractmethod
+    def parse_category_shop_response(self, body):
+        pass
+
+    @abstractmethod
+    def parse_city_shop_response(self, body):
+        pass
+
+    @abstractmethod
+    def get_dict_with_category_shop(self) -> dict:
         pass
